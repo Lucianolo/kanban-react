8341693cd587e3d5d16cb797d574fd6a36a4c0cc
Lucianolo <luciano.ursache@gmail.com>
2019-10-31 12:44:20 +0100

    Define data model, generate mock data and render tasks

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,6 +1,41 @@
 import React, { Component } from 'react'
 import Column from './Column'
+
+const TICKET_STATUSES = ['To-Do', 'In Progress', 'Done']
+
+const MOCK_DATA = [
+  {id: 1, status: 'To-Do', description: 'Do Something', persisted: true},
+  {id: 2, status: 'To-Do', description: 'Do Something', persisted: true},
+  {id: 3, status: 'To-Do', description: 'Do Something', persisted: true},
+  {id: 4, status: 'In Progress', description: 'Doing something', persisted: true},
+  {id: 5, status: 'Done', description: 'Did Something', persisted: true},
+  {id: 6, status: 'To-Do', description: 'Do Something', persisted: true},
+  {id: 7, status: 'To-Do', description: 'Do Something', persisted: true},
+  {id: 8, status: 'Done', description: 'Did Something', persisted: true},
+  {id: 9, status: 'In Progress', description: 'Doing something', persisted: true},
+]
 class Board extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      tasks: MOCK_DATA
+    }
+  }
+
+  addTask (status) {
+    const task = {
+      id: this.state.tasks[this.state.tasks.length - 1].id + 1,
+      status: status,
+      description: 'Add a description',
+      persisted: false,
+    }
+    this.setState((state) => {
+      return {
+        tasks: [...state.tasks, task],
+      }
+    })
+  }
+
   render() {
     return (
       <div className='Board'>
@@ -8,9 +43,14 @@ class Board extends Component {
           (Mini)Kanban board
         </div>
         <div className='columns horizontal'>
-          <Column name={'To-Do'}/>
-          <Column name={'In Progress'}/>
-          <Column name={'Done'}/>
+          {TICKET_STATUSES.map((status, index) => (
+            <Column
+              addTask={this.addTask.bind(this)}
+              name={status}
+              key={index}
+              tasks={this.state.tasks.filter((task) => task.status === status)}
+            />
+          ))}
         </div>
       </div>
     )
--- src/components/Column.js
+++ src/components/Column.js
@@ -1,10 +1,28 @@
 import React from 'react'
 
-const Column = ({ name }) => {
+const Column = ({ name, addTask, tasks }) => {
   return (
     <div className='Column vertical'>
-      <div className='column-name'>
-        {name}
+      <div className='column-header'>
+        <div className='column-name'>
+          {name}
+        </div>
+        <div
+          className='add-task-button'
+          onClick={() => addTask(name)}
+        >
+          +
+        </div>
+      </div>
+      <div className='column-body'>
+        {tasks.map((task) => (
+          <div
+            className='task-box'
+            key={task.id}
+          >
+            {task.description}
+          </div>
+        ))}
       </div>
     </div>
   )
